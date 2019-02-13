1d29dd3a5c3577b4823b82b79e49f9c5bf731f55
MageshDominator <mageshwaran951@gmail.com>
2019-02-13 21:47:49 +0530

    Added functionalities

--- PythonTools/mat2CSV/mat2CSV.py
+++ PythonTools/mat2CSV/mat2CSV.py
@@ -8,6 +8,7 @@ Created on Wed Feb  6 12:54:11 2019
 import scipy.io
 import pandas as pd
 import configparser
+import os
 
 # reading config.inf file
 config = configparser.ConfigParser()
@@ -17,47 +18,60 @@ section = config.sections()
 
 # storing section names into a list
 path = config.options(section[0])
-array = config.options(section[1])
- 
+
 # Extracting i/o filepath using config parser
-input_file = config.get(section[0], path[0])
-output_file = config.get(section[0], path[1])
+input_path = config.get(section[0], path[0])
+output_path = config.get(section[0], path[1])
+input_files = []
+output_files = []
 
-# stored as a dictionary
-mat = scipy.io.loadmat(input_file)
+# walk through the path and identify ".mat" files
+for root, _, filenames in os.walk(input_path):
+    # print(root)
+    for filename in filenames:
+        if filename.endswith(".mat"):
+            pathiter = os.path.join(root, filename)
+            input_files.append(pathiter)
+            out_file = os.path.join(output_path, filename)
+            newname =  out_file.replace('.mat', '.csv')
+            output_files.append(newname)
 
-# Extracting matrix names(used in .mat file) through config parser
-x_in = config.get(section[1], array[0])
-y_out = config.get(section[1], array[1])
+print(output_files)
+for i in range(len(input_files)):
+    input_file = input_files[i]
+    output_file = output_files[i]
 
-print(mat.keys())
+    # stored as a dictionary
+    mat = scipy.io.loadmat(input_file)
 
-# If not possible to read the .mat file in your system,
-# ------------ Manually Do the following -----------
-# 1. Get the matrix names through the above print statement
-# 2. modify x_in and y_out with respecting matrix_names (as string)
-# Example: x_in = "X" and y_out = "y"
-# ------------------------------------------------------
+    print(mat.keys())
 
-# convert it to a list
-X = mat[x_in].tolist()
-y = mat[y_out].tolist()
+    # If not possible to read the .mat file in your system,
+    # ------------ Manually Do the following -----------
+    # 1. Get the matrix names through the above print statement
+    # 2. modify x_in and y_out with respecting matrix_names (as string)
+    # Example: x_in = "X" and y_out = "y"
+    # ------------------------------------------------------
 
-column_names = []
+    # convert it to a list
+    X = mat["X"].tolist()
+    y = mat["y"].tolist()
 
-# add all the features and output in single list
-for i in range(len(X)):
-    X[i].append(y[i])
+    column_names = []
 
-# creating namelist for features and output columns
-for i in range(len(X[0])):
-    if i < len(X[0]) - 1:
-        column_names.append("X" + str(i))
-    else:
-        column_names.append("Y")
+    # add all the features and output in single list
+    for i in range(len(X)):
+        X[i].append(y[i][0])
 
-# covert that to a pandas dataframe
-data = pd.DataFrame(X, columns = column_names)
+    # creating namelist for features and output columns
+    for i in range(len(X[0])):
+        if i < len(X[0]) - 1:
+            column_names.append("X" + str(i))
+        else:
+            column_names.append("Y")
 
-# Save the dataframe as a CSV file
-data.to_csv(output_file, sep = ",", )
+    # covert that to a pandas dataframe
+    data = pd.DataFrame(X, columns = column_names)
+
+    # Save the dataframe as a CSV file
+    data.to_csv(output_file, sep = ",")
